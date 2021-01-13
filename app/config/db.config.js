77c4d5741a2ba0949bae8c12acaff790167e858c
relfarizi <46832182+relfarizi@users.noreply.github.com>
2021-01-13 15:44:36 +0700

    fix database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST : "localhost",
-    USER : "postgres",
-    PASSWORD : "bismillah123",
-    DB : "db_kompas",
+    HOST : "ec2-52-20-66-171.compute-1.amazonaws.com",
+    USER : "jkjxizrudfsybr",
+    PASSWORD : "9b1335b3e32ada56778423bff9a1cd236dad70dd0ab4e76c380e5eb8866c1176",
+    DB : "d2n5o2cmq717hu",
     dialect : "postgres",
     pool : {
         max: 5,
